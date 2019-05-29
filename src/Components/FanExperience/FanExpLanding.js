0311fe9503d4ee51bd5502b6f85a0860636f4715
Rayhan96Shuvo <rayhan96shuvo@gmail.com>
2019-05-29 18:52:29 -0500

    Landing page initially done. need to work with images to maintain ratio

--- src/Components/FanExperience/FanExpLanding.js
+++ src/Components/FanExperience/FanExpLanding.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import { Image } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
 
 import DaBike from '../../Assets/Image/DaveBike.jpg'
 import DaDaveOne from '../../Assets/Image/4.jpg'
+import DaDaveTwo from '../../Assets/Image/1.jpg'
+import DaDaveThree from '../../Assets/Image/3.jpg'
+import DaDaveFour from '../../Assets/Image/2.jpg'
 
 import '../../Assets/Stylesheet.css'
 
@@ -36,6 +40,7 @@ class FanExpLanding extends React.Component {
                 </a>
               </div>
               <br />
+
               <a href='/' className='brandRed'>
                 #
               </a>
@@ -66,7 +71,7 @@ class FanExpLanding extends React.Component {
             {' '}
             Dave is a RAAM veteran and winning is his Quest. And he's a man on
             the ascent. Dave's history with RAAM spans 2004, '05, '06, '08 and
-            then, after a seven-year hiatus finishes secone-place in both 2015
+            then, after a seven-year hiatus finishes second-place in both 2015
             and 2016.{' '}
           </p>
         </div>
@@ -80,25 +85,82 @@ class FanExpLanding extends React.Component {
             </div>
 
             <div class='col'>
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
+              <br />
+              <div className='mm'> Strength {'&'} Motivation </div>
+              <br />
+              <div className='nn'> Drive to Succeed</div>
+              <br />
 
-            <a href='/' className='brandWhite'>
-            Working...
-          </a>
+              <div className='container'>
+                <br />
+
+                <p className='textWhite'>
+                  {' '}
+                  2019 is Dave's 7th bid to win Five times, he's been top
+                  American finisher. His motivation? Simply, it's the perfect
+                  race.{' '}
+                </p>
+                <br />
+              </div>
             </div>
           </div>
         </div>
-        <div className="BLL">
-        Something
+
+        <div className='BLL'>
+          <Container className='SL '>
+            <Row>
+              <Col>
+                <br />
+                <h1 className='brandWhite'>From Pacific </h1>
+                <h1 className='brandWhite'> To Atlantic</h1>
+                <p className='textWhite1'>
+                  {' '}
+                  Called 'the toughest test of endurance in the world' by
+                  Outside magazine. The 3,000 miles course crosses 4 mountain
+                  range, 3 deserts and 170,000 vertical feet of climbing.{' '}
+                </p>
+              </Col>
+              <Col>
+                <div className='container'>
+                  <Image src={DaDaveTwo} fluid />
+                </div>
+              </Col>
+              <Col>
+                <div className='container'>
+                  <br />
+                  <br />
+                  <br />
+                  <Image src={DaDaveThree} fluid />
+                </div>
+              </Col>
+            </Row>
+          </Container>
+          <div>
+            <Row>
+              <Col>
+                <div className='container'>
+                  <Image src={DaDaveFour} fluid />
+                </div>
+              </Col>
+              <Col xs={7}>
+                <br />
+                <div className='LLL container'>
+                  <div className='mm'> Breakthrough </div>
+                  <br />
+                  <div className='nn'> Technology </div>
+                  <br />
+                  <p className='textWhite1'>
+                    {' '}
+                    Enabling this insanity is a technical internet of things
+                    platform to explore that 'line' that seperates breakthrough
+                    from catastrophe.{' '}
+                  </p>
+                </div>
+              </Col>
+            </Row>
+            <br />
+          </div>
         </div>
-
-
-
       </div>
     )
   }
