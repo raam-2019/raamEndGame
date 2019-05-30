2cc48d87e1af3e371a37a95276c4994c14061784
JonasKlare <jonasklare4@aol.com>
2019-05-29 21:43:11 -0600

    Added a single graph to widget2.  There is some functionality that needs to be added still, such as responsiveness, but that will grow as our framework does.  We also need to plug in real data and set up our xy axis, but those are just tags.  For documentation we used, see comments

--- src/Components/Widget2.js
+++ src/Components/Widget2.js
@@ -1,7 +1,18 @@
 import React, {Component} from 'react';
 import { Container } from "reactstrap";
+import {curveCatmullRom} from 'd3-shape';
+import {XYPlot, XAxis, YAxis, VerticalGridLines, HorizontalGridLines, LineSeries} from 'react-vis';
 
-class Widget2 extends React.Component {
+//Attempting to autofill the size with these flexible imports
+//https://github.com/uber/react-vis/blob/master/docs/flexible-plots.md
+import {
+  FlexibleXYPlot,
+  FlexibleWidthXYPlot,
+  FlexibleHeightXYPlot
+} from 'react-vis';
+
+
+export default class Widget2 extends React.Component {
 
 
     constructor(props) {
@@ -16,16 +27,42 @@ class Widget2 extends React.Component {
             this.spanStyles.gridRow = `span ${props.rowspan}`;
         }
     }
-    
+
+
     render() {
+
+      //Constants from for setting up canvas
+      //const {useCanvas} = this.state;
+      //const content = useCanvas ? 'TOGGLE TO SVG' : 'TOGGLE TO CANVAS';
+      //const Line = useCanvas ? LineSeriesCanvas : LineSeries;
+
+      //Style of this will default to fill.  To fix this bug, look at
+      //https://github.com/uber/react-vis/issues/757
+
+      //This plot is from this tutorial
+      //https://medium.com/dailyjs/data-visualization-with-react-vis-bd2587fe1660
+
         return (
            <div style={this.spanStyles} className="Widget">
                 <div className="header">
                     <h2>
                         {this.props.heading}
                     </h2>
-                    {/* Conditionally show the loading spinner */}
-                    {/* {this.props.loading ? <Loading /> : ""} */}
+
+                    <FlexibleXYPlot
+                      width={300}
+                        height={300}>
+                        <VerticalGridLines />
+                        <HorizontalGridLines />
+                        <XAxis />
+                        <YAxis />
+                        <LineSeries
+                          data={[
+                              {x: 1, y: 4},
+                              {x: 5, y: 2},
+                              {x: 15, y: 6}
+                          ]}/>
+                    </FlexibleXYPlot>
                 </div>
                 <div className="content">
                     {this.props.children}
@@ -34,5 +71,3 @@ class Widget2 extends React.Component {
         );
     }
 }
-
-export default Widget2
