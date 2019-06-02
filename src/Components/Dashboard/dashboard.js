abb9285e196c17d6acc95843b3cce213cb3e3dc5
JonasKlare <jonasklare4@aol.com>
2019-06-01 22:24:12 -0600

    fleshed out a bit more of the teamxp dashboard leveraging the same map component of the fan experience. May be able to use a little bit more of the other components we will set up in the fanxp

--- src/Components/Dashboard/dashboard.js
+++ src/Components/Dashboard/dashboard.js
@@ -7,10 +7,19 @@ import { API, graphqlOperation } from "aws-amplify";
 //Internal
 import { Racerdata, Asset } from "../../graphql/queries";
 import Navagation from "../GlobalUtilities/Navigation";
+
+import Widget0 from "../Widget0"
+
 import Widget1 from "./teamxpWidget1"
 import Widget2 from "./teamxpWidget2"
 import Widget3 from "./teamxpWidget3"
+
+import Widget4 from "./teamxpWidget4"
+import Widget5 from "./teamxpWidget5"
+import Widget6 from "./teamxpWidget6"
+
 import "../../Assets/BiometricsStyle.css";
+import "../../Assets/OptimizationStyle.css";
 
 class Dashboard extends React.Component {
 
@@ -47,10 +56,19 @@ class Dashboard extends React.Component {
           </h1>
           <br />
 
+          <Widget0 />
+          <br />
+
           <div className="biometrics">
             <Widget1 heading="Stat 1" rowspan={2} colspan={1} />
             <Widget2 heading="Stat 2" rowspan={2} colspan={1} />
-            <Widget3 heading="Some graph shit" colspan={1} rowspan={2} />
+            <Widget3 heading="Some graph shit" rowspan={2} colspan={1} />
+          </div>
+
+          <div className="optimization">
+            <Widget4 heading="optimization" rowspan={2} colspan={4} />
+            <Widget5 heading="power & speed" rowspan={2} colspan={4} />
+            <Widget6 heading="Batteries and stuff" rowspan={1} colspan={4} />
           </div>
         </div>
       );
