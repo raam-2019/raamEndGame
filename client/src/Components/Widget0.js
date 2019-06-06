8e595437fd06fac53d14413f28af06c6d84644f0
NafisFaisalKhan <nafisfaisalkhan@gmail.com>
2019-06-06 15:53:03 -0500

    adding trackleader data to map

--- client/src/Components/Widget0.js
+++ client/src/Components/Widget0.js
@@ -1,21 +1,17 @@
 
 import React, {Component} from 'react';
-// import Navagation from "../Navagation";
 import { Container } from "reactstrap";
-// import React from 'react'
 import ReactMapGL, { Marker, Popup, NavigationControl,LinearInterpolator, FlyToInterpolator } from 'react-map-gl';
-// import ReactMapGL from 'react-map-gl';
-// import { Component } from 'react';
-// import Pin from './Pin'
-// import './fanexperience.css';
-// 3rd-party easing functions
 // import d3 from 'd3-ease';//Mapbox token
-const TOKEN = 'pk.eyJ1IjoibWlzbGFtNSIsImEiOiJjanVpdG5vZWoxZThsNGZwamJ4Nmxya2o0In0.19pBli659L76GrJaX0JWoA';
+import {fromJS} from 'immutable';
+// const TOKEN = 'pk.eyJ1IjoibWlzbGFtNSIsImEiOiJjanVpdG5vZWoxZThsNGZwamJ4Nmxya2o0In0.19pBli659L76GrJaX0JWoA'; //rayhan's token
+const TOKEN = 'pk.eyJ1IjoicnNiYXVtYW5uIiwiYSI6ImNqdzg5aWxkYzF1azI0OW5uaWVmazhleXUifQ.XAm1dRGmXuRAMSQm0TJKXg'; //ryan baumann's token
 //Mapbox Navigation Style
 
 class Widget0 extends React.Component {
 
     state = {
+        mapStyle:'mapbox://styles/rsbaumann/cjwdv1evs18rk1cnuwku6tys5?optimize=true',
         viewport: {
             width: '70%',
             height: 500,
@@ -26,10 +22,20 @@ class Widget0 extends React.Component {
             pitch: 0,
         },
 
-        riderData : []
-        
+        riderData : [],
+
+        pointLayer: fromJS({
+            id: 'point',
+            source: 'point',
+            type: 'circle',
+            paint: {
+              'circle-radius': 10,
+              'circle-color': '#007cbf'
+            }
+          })
     };
 
+    
     _onViewportChange = viewport => {
         this.setState({viewport});
     };
@@ -51,8 +57,7 @@ class Widget0 extends React.Component {
           console.log(this.props);
     }
 
-    componentDidMount(){
-      
+    componentDidMount(){ 
         this.trackData();
      }
  
@@ -61,30 +66,69 @@ class Widget0 extends React.Component {
          setInterval(function(){
              fetch('/api/trackleads')
              .then(res => res.json())
-             .then(data => console.log(data))
+             .then(function(data){
+
+                 let {mapStyle} = this.state;
+
+                 var trackleaders = data.trackleaders_aggregate_feed.trackleaders_feed;
+                 var pointData ;
+                 var points = [];
+                 
+                 trackleaders.forEach(trackleader => {
+                    pointData = {
+                        type: 'Point',
+                        coordinates: [parseFloat(trackleader.message[0].longitude[0]) , parseFloat(trackleader.message[0].latitude[0])]
+                    };
+
+                    points.push(pointData)
+
+                });
+
+                if (!mapStyle.hasIn(['sources', 'point'])) {
+                    mapStyle = mapStyle
+                      // Add geojson source to map
+                      .setIn(['sources', 'point'], fromJS({type: 'geojson'}))
+                      // Add point layer to map
+                      .set('layers', mapStyle.get('layers').push(this.state.pointLayer));
+                  }
+
+                mapStyle = mapStyle.setIn(['sources', 'point', 'data'], points );
+
+                this.setState({mapStyle});
+
+                // console.log(parseFloat(trackleaders[0].message[0].latitude[0]));
+             })
              .catch(function(error) {
                  console.log('Looks like there was a problem: \n', error);
              })
-         } , 60000) ;
+         } , 10000) ;
          
      }
-    // mapStyle = "mapbox://styles/mislam5/cjuiyejbm6qn11gnv0e44i7qm"
+
     // For Dashboard access for now we will have to add /dashboardRAAMforVIPaccess to our http
     //Fanexperience will be root dir
     render() {
         return (
             <div id="mainWrapper">
+                
                 <Container className="d-flex flex-wrap justify-content-left align-items-center align-content-center">
+                    
                     <ReactMapGL mapboxApiAccessToken={TOKEN}
-                        // mapStyle="mapbox://styles/mislam5/cjuiyejbm6qn11gnv0e44i7qm"
-                        mapStyle='mapbox://styles/mislam5/cjuiyejbm6qn11gnv0e44i7qm'
-                        {...this.state.viewport} onViewportChange={this._onViewportChange}
+                        mapStyle={this.state.mapStyle}
+                        //mapStyle='mapbox://styles/rsbaumann/cjwdycid51bvk1cp7ye1y0u5f?optimize=true'
+                        {...this.state.viewport} 
+                        onViewportChange={this._onViewportChange}
+                        
                      >
-                        <div className="nav">
-                            <NavigationControl onViewportChange={(viewport) => this.setState({ viewport })}  />   
-                        </div>
+
+                    <div className="nav">
+                        <NavigationControl onViewportChange={(viewport) => this.setState({ viewport })}  />   
+                    </div>
+                    
                     </ReactMapGL>
+
                     <button onClick={this._goToCyclist}>Cyclist</button>
+                
                 </Container>
             </div>
         );
