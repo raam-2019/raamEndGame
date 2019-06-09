84f0d13e2e06764db42fd3eba7264df0f95f5b8b
Andrew Geng <andrew.geng@Andrews-MacBook-Pro.local>
2019-06-08 20:04:08 -0700

    Added styles on Jonas' request

--- src/components/NumberWidget/NumberWidget.tsx
+++ src/components/NumberWidget/NumberWidget.tsx
@@ -5,14 +5,20 @@ import styles from "./NumberWidget.module.css";
 
 export interface INumberWidgetProps {
   numberPoints: IPoint[];
-
+  name: String;
   extraClassName?: string;
 }
 
 export const NumberWidget: React.FC<INumberWidgetProps> = props => (
   <div className={`${styles.root} ${props.extraClassName || ''}`}>
     {props.numberPoints.length > 0 ? (
-      <strong className={styles.numberStyle}>{props.numberPoints[props.numberPoints.length - 1].y}</strong>
+      <strong className={styles.numberStyle}>
+      {props.name === "temp" ? (
+        props.numberPoints[props.numberPoints.length - 1].y.toString().substring(0, 4) + '\xB0C'
+      ) : (
+        props.numberPoints[props.numberPoints.length - 1].y.toString().substring(0, 4) + '%'
+      )}
+      </strong>
     ) : (
       <LoadingOverlay />
     )}
