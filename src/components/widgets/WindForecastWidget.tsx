778fc69db5e59c73bba9b60a3804ea6abc36211a
JonasKlare <jonasklare4@aol.com>
2019-06-10 21:22:19 -0600

    Ope forgot to save this

---
+++ src/components/widgets/WindForecastWidget.tsx
@@ -0,0 +1,55 @@
+import * as React from 'react';
+
+import {XYPlotTemplate} from 'components/widgets/shared/XYPlotTemplate';
+import {IDefaultWidgetProps} from 'components/widgets/shared/IDefaultWidgetProps';
+import {
+  BasicHorizontalAxis,
+  BasicVerticalAxis
+} from 'components/widgets/shared/BasicAxes';
+import {LinearAreaSeries} from 'components/widgets/shared/BasicAreaSeries';
+import * as util from './shared/util';
+
+
+export interface IWindForecastWidget extends IDefaultWidgetProps {
+  elevationData: IPoint[];
+  forecastedWind: IPoint[];
+}
+//TODO(klare): add in and statements into the status.
+//TODO(klare): integrate actual live data and see how it looks.
+
+export const WindForecastWidget: React.FC<IElevationWidgetProps> = props => (
+  <XYPlotTemplate
+    heightPx={props.heightPx}
+    widthPx={props.widthPx}
+    status={props.data.length > 2 && props.data.length > 2 ? 'ready' : 'loading'}
+    title="Headwind vs 2hr forecasted headwind"
+    useHorizontalGridLines={true}>
+
+    {LinearAreaSeries({
+      data: props.elevationData;
+      lineColor: 'orange',
+      fillColor: '#FFCF9E',
+      lineWidthPx: util.StrokeWidthPx
+    })}
+
+    {LinearLineSeries ({
+      data: props.forecastedWind,
+      lineColor: 'red',
+      lineWidthPx: util.StrokeWidthPx
+    })}
+
+    {LinearLineSeries ({
+      data: props.data,
+      lineColor: 'blue',
+      lineWidthPx: util.StrokeWidthPx
+    })}
+
+    {BasicHorizontalAxis({
+      axisLabel: "Time (Minutes)",
+    })}
+
+    {BasicVerticalAxis({
+      axisLabel: "Headwind Speed"
+    })}
+  </XYPlotTemplate>
+);
